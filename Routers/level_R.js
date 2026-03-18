35b1af3e896dcb88b438965345da89c805b3dfe5
mhmdkhateeb74 <mhmdkhateeb92@gmail.com>
2026-03-19 01:41:03 +0200

    Level midd/router

---
+++ Routers/level_R.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const router = express.Router();
+module.exports = router;
+
+const level_Mid = require("../Middlewares/level_Mid");
+
+router.post("/Add", [level_Mid.AddItem], (req, res) => {
+    if(res.ok)
+        res.status(200).json({message:"OK", Last_Id:res.insertId});
+    else
+        return res.status(500).json({message: res.err});
+});
+
+router.put("/Update/:id", [level_Mid.UpdateItem], (req, res) => {
+    if(res.ok)
+        res.status(200).json({message:"OK"});
+    else
+        return res.status(500).json({message: res.err});
+});
+
+router.delete("/Delete", [level_Mid.DeleteItem], (req, res) => {
+    if(res.ok)
+        res.status(200).json({message:"OK"});
+    else
+        return res.status(500).json({message: res.err});
+});
+
+router.get("/List", [level_Mid.GetAllItems], (req, res) => {
+    if(res.ok) {
+        res.status(200).json(req.ItemsData);
+    }
+    else
+        return res.status(500).json({message: res.err});
+});
